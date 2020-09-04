c011496bc2489f091286ffa7dab5c91b7f99bf23
benftima <benftima.mohamed@gmail.com>
2020-09-04 14:00:50 +0200

    fist server commit

---
+++ src/main/java/tn/logarihme/ServerGspApplication.java
@@ -0,0 +1,13 @@
+package tn.logarihme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/logarihme/ServerGspApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.logarihme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
